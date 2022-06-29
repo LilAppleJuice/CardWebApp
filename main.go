bc925bdbc806483745981a56d980b006aa88dd2c
LilAppleJuice <mtmcreeper@gmail.com>
2022-06-28 23:56:12 -0500

    setup mongodb added MVC setup

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"net/http"
+	//"net/http"
 	"time"
-	"C:\Program Files\Go\src\Users\mtmcr\Documents\CS\GoLang\first_website\cards"
+	"context"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	//"home/ubuntu/Documents/CS/CardWebApp/cards"
+
 )
+
+/*
+func userChoice() char {
+	fmt.Println("Enter command: ")
+	var userC char
+	fmt.Scanf(&userC)
+	return userC
+}
+*/
 
 func close(client *mongo.Client, ctx context.Context, cancel context.CancelFunc) {
 	defer cancel()
@@ -25,6 +39,28 @@ func connect(uri string) (*mongo.Client, context.Context, context.CancelFunc, er
 	return client, ctx, cancel, err
 }
 
+func ping(client *mongo.Client, ctx context.Context) error{
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return err
+	}
+	fmt.Println("connected successfully")
+	return nil
+}
+
+func insertOne(client *mongo.Client, ctx context.Context, dataBase, col string, doc interface{}) (*mongo.InsertOneResult, error) {
+	collection := client.Database(dataBase).Collection(col)
+	result, err := collection.InsertOne(ctx, doc)
+	return result, err
+}
+
+/*
+func insertMany(client *mongo.Client, ctx context.Context, dataBase, col string, docs []interface{}) (*mongo.InsertManyResult, error) {
+    collection := client.Database(dataBase).Collection(col)
+    result, err := collection.InsertMany(ctx, docs)
+    return result, err
+}
+*/
+
 func main() {
 	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	//	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
@@ -35,5 +71,53 @@ func main() {
 
 	//http.ListenAndServe(":80", nil)
 
-	cards.second.main()
+	client, ctx, cancel, err := connect("mongodb+srv://admin:mtmE823tv12@cluster0.euonptj.mongodb.net/?retryWrites=true&w=majority")
+	if err != nil {
+		panic(err)
+	}
+	defer close(client, ctx, cancel)
+
+	var document interface{}
+	var usrN string
+	var passwd string
+	var tstCrd string
+	usrN = "test"
+	passwd = "test"
+	tstCrd = "test"
+
+	document = bson.M {"username": usrN, "password": passwd, "cards": bson.M {"testCard": tstCrd}}
+
+	insertOneResult, err := insertOne(client, ctx, "CardApp", "Users", document)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Result of InsertOne")
+	fmt.Println(insertOneResult.InsertedID)
+
+	//ping(client, ctx)
+	/*
+
+	var userC char
+	var isDone bool
+	isDone := false
+	while isDone == false {
+		userChoice(&userC)
+		if userC == 'h' {
+			fmt.Println("\nh for help\nc to make a new card\nl to list all cards")
+		} else if userC == 'c' {
+			isDone := true
+			cards.MakeCard("front of card", "back of card", true, false)
+		} else if userC == 'l' {
+			isDone := true
+			cards.ListCards()
+		} else {
+			fmt.Println("Invalid Command Entered, Try again: ")
+		}
+	}
+	fmt.Println("Closing Program...")
+
+	userChoice()
+	*/
 }
